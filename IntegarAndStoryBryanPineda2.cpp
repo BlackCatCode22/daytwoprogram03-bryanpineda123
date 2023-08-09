ae7e68f78e8c1d9a9f91924763b5bfb08cc985b3
bryan <141785744+bryanpineda123@users.noreply.github.com>
2023-08-09 12:06:34 -0700

    Add files via upload

---
+++ IntegarAndStoryBryanPineda2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    // cin >> num; // this is a input command
+    //ChatGPT Adding Method
+    int num1, num2;
+
+    num1 = 6;
+    num2 = 6;
+    int sum2 = num1 + num2;
+    cout << "these numbers add up to " << sum2 << endl;
+
+    // Different Adding method
+    int number1 = 5;
+    int number2 = 5;
+    int sum = number1 + number2;
+    cout << "these numbers add up to " << sum << endl;
+
+    // Story
+    string characterName = "John";
+    int characterAge;
+    characterAge = 50;
+
+    cout << characterName << " is " << characterAge << " years old " << endl;
+    cout << characterName << " does not like being " << characterAge << " years old " << endl;
+    cout << characterName << " is " << characterAge << " years old " << endl;
+    cout << characterName << " does not like being " << characterAge << " years old " << endl;
+
+    return 0;
+
+}
